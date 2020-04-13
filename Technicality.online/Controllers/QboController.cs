c7339795f56e1efe79fad1cef1a75e6af7222d67
Jeff Trotman <jeff@jefftrotman.com>
2020-04-13 11:39:49 -0500

    Qbo EULA and Privacy

---
+++ Technicality.online/Controllers/QboController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Technicality.online.Controllers
+{
+    public class QboController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EULA()
+        {
+            return View();
+        }
+
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
